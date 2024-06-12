1d24d44798410f88cde713694247083812e21ad5
Dan <danhollard@gmail.com>
2024-06-12 19:44:45 +0100

    created simple script in hello world.py to pull a protein reference ID file from the protein databank, store it and display the contents of the file. Code is basic and not polished

--- hello world.py
+++ hello world.py
@@ -1,1 +1,16 @@
-print("hello world")
+import requests
+
+
+def download_pdb(pdb_id, save_path): #downloads a pdb file from the pdb website and writes it to a text file
+    url = f'https://files.rcsb.org/download/{pdb_id}.pdb'
+    response = requests.get(url)
+    with open(save_path, 'w') as file:
+        file.write(response.text)
+
+pdb_id = '1A8M'  #my ID of choice (protein: TNF-alpha)
+filename = download_pdb(pdb_id, f'{pdb_id}.pdb')
+
+f = open(str(pdb_id) + '.pdb', 'r')
+file_contents = f.read()
+print(file_contents)
+f.close
